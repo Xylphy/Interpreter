13955ff299b1b394d81ae7fdd257f090d1025897
Xylphy <Fleurette>
2025-02-21 18:00:24 +0800

    Added support for int & double

--- src/utility.cpp
+++ src/utility.cpp
@@ -1,8 +1,9 @@
 #include "Headers/Lib/utility.hpp"
 
+#include <any>
 #include <functional>
+#include <string>
 #include <typeindex>
-#include <unordered_map>
 
 namespace utility {
 
@@ -31,5 +32,38 @@ auto anyToString(std::string& result, const std::any& value) noexcept -> bool {
     return true;
   }
   return false;
+}
+
+auto isConvertibleToString(const std::any& value) -> bool {
+  const std::type_info& type = value.type();
+  return isArithmetic(value) || type == typeid(std::string) ||
+         type == typeid(char);
+}
+
+auto isArithmetic(const std::any& value) -> bool {
+  const std::type_info& type = value.type();
+  return type == typeid(int) || type == typeid(double);
+}
+
+auto checkValidOperation(const std::any& left, TokenType type,
+                         const std::any& right) -> bool {
+  switch (type) {
+    case TokenType::GREATER:
+    case TokenType::GREATER_EQUAL:
+    case TokenType::LESS:
+    case TokenType::LESS_EQUAL:
+    case TokenType::BANG_EQUAL:
+    case TokenType::EQUAL_EQUAL:
+    case TokenType::MINUS:
+    case TokenType::PLUS:
+    case TokenType::SLASH:
+    case TokenType::STAR:
+    case TokenType::MODULO:
+      return isArithmetic(left) && isArithmetic(right);
+    case TokenType::CONCATENATOR:
+      return isConvertibleToString(left) && isConvertibleToString(right);
+    default:
+      return false;
+  }
 }
 }  // namespace utility
